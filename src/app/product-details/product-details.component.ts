5d66bd8e0a67c50e6f2747225723ba3cf4577c05
CetinkCi <cigdem_cetinkaya@hotmail.de>
2019-06-11 11:37:43 +0200

    3 files modified, 7 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { products } from '../products';
+import {CartService} from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -15,8 +15,15 @@ ngOnInit() {
     this.product = products[+params.get('productId')];
   });
 }
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService,
   ) { }
+  
 
 }
